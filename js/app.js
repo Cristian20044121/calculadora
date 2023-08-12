2ba35463d53d7208b3bd1c4e4e652f48d868f002
Cristian Caro <crdcaro01@gmai.com>
2023-08-11 20:18:03 -0500

    agregado de estilos

--- js/app.js
+++ js/app.js
@@ -1,27 +1,26 @@
 const contenedor = document.querySelector('.container');
 const buttons = document.querySelectorAll('.button');
 const display = document.getElementById('display');
-console.log(contenedor)
+display.value = 0;
 
-
-// Agregamos un manejador de clic para cada botón
 buttons.forEach(button => {
     button.addEventListener('click', () => {
         const buttonText = button.textContent;
 
         if (buttonText === '=') {
-            // Realizar el cálculo cuando se presione el botón '='
             try {
                 display.value = eval(display.value);
             } catch (error) {
                 display.value = 'Error';
             }
         } else if (buttonText === 'C') {
-            // Limpiar la pantalla cuando se presione el botón 'C'
-            display.value = '';
+            display.value = '0';
         } else {
-            // Agregar el número u operador al campo de entrada
-            display.value += buttonText;
+            if (display.value === '0') {
+                display.value = buttonText;
+            } else {
+                display.value += buttonText;
+            }
         }
     });
 });
